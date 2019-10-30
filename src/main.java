db7afa0771632ce3084af6680a4f9f5ebd7e1bf0
Michael <micheallam@csu.fullerton.edu>
2019-10-30 14:43:29 -0700

    name

--- src/main.java
+++ src/main.java
@@ -1,4 +1,9 @@
-
+/*
+ * Michael Lam
+ * 10/30/19
+ * Lab # 14
+ * Makes a simple casino-like dice game in Java's Border Layout
+ */
 public class main {
 
 	public static void main(String[] args) {
